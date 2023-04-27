5097a4d1908e9aba352e89111b006e676656c9a0
tkgstrator <nasawake.am@gmail.com>
2023-04-28 00:59:02 +0900

    add chart.js

---
+++ src/components/LineChart.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+} from 'chart.js'
+import { Line } from 'vue-chartjs'
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+)
+
+const data = {
+  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  datasets: [
+    {
+      label: 'Data One',
+      backgroundColor: '#f87979',
+      data: [40, 39, 10, 40, 39, 80, 40]
+    }
+  ]
+}
+const options = {
+  responsive: true,
+  maintainAspectRatio: true
+}
+</script>
+
+<template>
+  <Line :data="data" :options="options" />
+</template>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,3 +1,9 @@
+<script setup lang="ts">
+import LineChart from '@/components/LineChart.vue';
+
+</script>
+
 <template>
   <RouterLink to="/schedules">Schedules</RouterLink>
+  <LineChart />
 </template>
