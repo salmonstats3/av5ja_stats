e8bb3b05483f6ab7c20102855723393bf6c74173
tkgstrator <nasawake.am@gmail.com>
2023-05-01 16:53:30 +0900

    update: resources

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ import json
 if __name__=="__main__":
   with open("src/resources/schedules.json", mode="r") as f:
     schedules = json.load(f)
-    for schedule in schedules:
+    for (index, schedule) in enumerate(schedules):
       try:
         url: str = f"http://localhost:8080/v3/schedules/{schedule['scheduleId']}"
         response = requests.get(url)
         if response.status_code != 200:
           print(response.status_code)
         else:
-          print(f"Success: {schedule['scheduleId']}")
+          print(f"\r{index+1}/{len(schedules)}", end='')
           with open(f"src/resources/schedules/{schedule['scheduleId']}.json", mode="w") as f:
             json.dump(response.json(), f)
       except Exception as e:
