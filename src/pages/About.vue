031b7e82785d27cb767e0bbfe88578b4a43c6a28
tkgstrator <nasawake.am@gmail.com>
2023-05-06 05:00:05 +0900

    update: service worker

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -11,8 +11,13 @@ const date: string = import.meta.env.VITE_APP_GIT_COMMIT_DATE
     <v-col cols="12">
       <v-card>
         <v-card-title>{{ t("VITE_APP.VERSION")  }}</v-card-title>
-        <v-card-subtitle>{{ hash }}</v-card-subtitle>
+        <v-card-subtitle>
+          {{ hash }}
+        </v-card-subtitle>
         <v-card-text>{{ t("VITE_APP.LAST_UPDATE_AT")}}: {{ date }}</v-card-text>
+        <v-card-text>
+          <v-img src="https://github.com/SalmonStats3/salmonstats-dev/actions/workflows/schedules-build.yml/badge.svg" width="160"></v-img>
+        </v-card-text>
       </v-card>
     </v-col>
     <v-col cols="12">
